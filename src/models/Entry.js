cc3dfdf1ba3c36488538e752b46bd617ba4ec95b
athenfam <athenfam@gmail.com>
2025-11-24 12:36:01 -0800

    add env file

---
+++ src/models/Entry.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const entrySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Entry = mongoose.model("Entry", entrySchema);
+
+export default Entry;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import Entry from "./models/Entry.js"
 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+//midlleware to parse JSON bodies
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.get("/", (req, res) => {
@@ -21,4 +25,28 @@ app.get("/api/login", (req, res) => {
     // verify login credentials here
     res.send("you are logged in");
 });
+
+app.post("/api/createEntry", async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const entry = new Entry({ title, content });
+        const savedEntry = await entry.save();
+        res.status(201).json(savedEntry);
+    } catch (error) {
+        console.error("Error creating entry ", error);
+    }
+});
+
+app.get("/api/getEntry", async (req, res) => {
+    try {
+        const entry = await Entry.find();
+        res.status(200).json(entry);
+    } catch (error) {
+        console.error("Error getting entry ", error);
+    }
+})
+
+app.put("/api/updateEntry/:id", async (req, res) => {
+    try
+})
 
