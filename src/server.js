b44812613d20079124c450aa59ef44929cbb113b
athenfam <athenfam@gmail.com>
2025-11-24 22:06:47 -0800

    add middleware and delete endpoint

--- src/server.js
+++ src/server.js
@@ -10,14 +10,22 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 
-//midlleware to parse JSON bodies
+//midlleware to parse JSON bodies: req.body
 app.use(express.json());
+
+// Simple logging middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();  
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.get("/", (req, res) => {
+// add _ if not used 
+
+app.get("/", (_, res) => {
     res.send("Hello World!");
 });
 
@@ -26,7 +34,7 @@ app.get("/api/login", (req, res) => {
     res.send("you are logged in");
 });
 
-app.post("/api/createEntry", async (req, res) => {
+app.post("/api/addEntry", async (req, res) => {
     try {
         const { title, content } = req.body;
         const entry = new Entry({ title, content });
@@ -34,6 +42,7 @@ app.post("/api/createEntry", async (req, res) => {
         res.status(201).json(savedEntry);
     } catch (error) {
         console.error("Error creating entry ", error);
+        res.status(500).json({ message: "Server Error" });
     }
 });
 
@@ -43,10 +52,43 @@ app.get("/api/getEntry", async (req, res) => {
         res.status(200).json(entry);
     } catch (error) {
         console.error("Error getting entry ", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+})
+
+app.get("/api/getEntry/:id", async (req, res) => {
+    try {
+        const entry = await Entry.findById(req.params.id);
+        res.status(200).json(entry);
+    } catch (error) {
+        console.error("Error getting entry ", error);
+        res.status(500).json({ message: "Server Error" });
     }
 })
 
 app.put("/api/updateEntry/:id", async (req, res) => {
-    
-})
+    try{
+        const { title, content } = req.body;
+        const updatedEntry = await Entry.findByIdAndUpdate(req.params.id, { title, content }, { new: true });
+        if (!updatedEntry) {
+            return res.status(404).json({ message: "Entry not found" });
+        }
+        res.status(200).json(updatedEntry);
+    } catch (error) {
+        console.error("Error updating entry ", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
 
+app.delete("/api/deleteEntry/:id", async (req, res) => {
+    try {
+        const deletedEntry = await Entry.findByIdAndDelete(req.params.id);
+        if (!deletedEntry) {
+            return res.status(404).json({ message: "Entry not found" });
+        }
+        return res.status(200).json("Deleted entry" + req.params.id);
+    } catch (error) {
+        console.error("Error deleting entry ", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
